e66e7cf290a9a0cef62f14f616ce2eedfbd0b18f
CodeWithme-Ali <imalimubashir2121@gmail.com>
2025-08-03 17:45:56 +0500

    Program to Reverse a number.

---
+++ rev_num.py
@@ -0,0 +1,9 @@
+# Program to Reverse a number.
+
+
+print("\n --- Reverse Number ---")
+num = int(input("Enter a number: "))
+
+rev = str(num)[::-1] # slicing Value
+
+print("Reversed number:", rev)
