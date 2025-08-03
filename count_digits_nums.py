229e08f7631c53a0ff134b31076f6ed8b4be08d4
CodeWithme-Ali <imalimubashir2121@gmail.com>
2025-08-03 17:44:22 +0500

    Program to count the number of digits in a number.

---
+++ count_digits_nums.py
@@ -0,0 +1,8 @@
+# Program to count the number of digits in a number. 
+
+print("\n--- Counts Digits in Number ---\n ")
+num = int(input("Enter a number: "))
+
+count = len(str(num))
+
+print("Number of digits:", count)
